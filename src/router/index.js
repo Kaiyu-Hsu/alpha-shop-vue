192af58e07c8656a964debf71f0f0aaa5b9271ae
Kaiyu <anne550323@gmail.com>
2021-09-05 21:36:26 +0800

    feat: delete Home route

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/main',
     name: 'Main',
-    component: () => import('../views/Main.vue')
-  }
+    component: Main
+  },
+  // {
+  //   path: '/main',
+  //   name: 'Main',
+  //   component: () => import('../views/Main.vue')
+  // }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="home">
-    <h1>動工中!!!!加油!!!</h1>
-    <img src="https://static.betweengos.com/wp-content/uploads/2017/01/stickers-designed-for-working-women-32.gif" alt="">
-  </div>
-</template>
-
-
